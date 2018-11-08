88b0de9ea1dc4127cda48a9e6172f3b32109daa6
Naminek <nam.k.0519@gmail.com>
2018-11-08 14:19:48 +0100

    styled home page

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row">
+    <div class="col-8">
+    </div>
+    <div class="col-4">
+	    <div class="d-flex justify-content-end header_icon">
+        <img alt="chat_icon" src="../assets/chat.png">
+		    <img alt="menu_icon" src="../assets/menu_bar.png" class="ml-1">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader"
+  // props: {
+  //   msg: String
+  // }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header_icon img {
+  width: 50%;
+  height: auto;
+  object-fit: contain;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home pl-3 pr-3">
+    <HomeHeader />
+
+<div class="d-flex justify-content-center" id="title">
+    <p class="h1 d-flex justify-content-center pb-3">Title</p>
+    </div>
+    <div id="logo">
+    <img alt="logo" src="../assets/premier_league_logo.png"
+    class="img-fluid mx-auto d-block mt-2">
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import HomeHeader from "@/components/HomeHeader.vue";
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "home",
+    components: {
+      HomeHeader
+    }
+  };
 </script>
+
+<style scoped>
+/* .home {
+  background-color: bisque;
+} */
+
+#title p {
+  width: 50%;
+  border-bottom: solid 3px rgba(56, 55, 55, 0.7);
+}
+#logo img{
+  width: 25%;
+}
+
+</style>
