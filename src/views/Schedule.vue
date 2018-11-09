2155f0e771fa07af5b2afcd47e399a2336279f99
Naminek <nam.k.0519@gmail.com>
2018-11-09 18:04:28 +0100

    set header

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -5,7 +5,13 @@
 		<div class="d-flex justify-content-center" id="title">
 			<p class="h4 d-flex justify-content-center pb-4"></p>
 		</div>
-		
+		<div id="search_engine" class="d-flex">
+			<label for="search_team">
+				<img src="../assets/scope.png" alt="search_icon"></label>
+			<input type="text" placeholder="search by team" name="search_team" size="15">
+			<input type="date" name="today" id="today">
+		</div>
+
 	</div>
 </template>
 
@@ -28,5 +34,18 @@
 		border-bottom: solid 3px rgba(56, 55, 55, 0.7);
 	}
 
+	#search_engine {
+		margin-top: 50px;
+	}
 
+	#search_engine input {
+		padding: 5px;
+		margin: 0 5px;
+		border: 1px solid rgba(56, 55, 55, 0.7);
+		border-radius: 4px;
+	}
+
+	#search_engine input {
+		width: 45%;
+	}
 </style>
